b922c5bbd28e0025302a8c272a559326f859718b
Kekeli Lit <agbagabenjamin819@gmail.com>
2024-07-31 08:37:20 +0000

    commit

---
+++ atulms.sql
@@ -0,0 +1,288 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 31, 2024 at 10:36 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `atulms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lms_admin`
+--
+
+CREATE TABLE `lms_admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_email` varchar(200) NOT NULL,
+  `admin_password` varchar(225) NOT NULL,
+  `admin_username` varchar(225) NOT NULL,
+  `admin_contact_no` varchar(30) NOT NULL,
+  `admin_profile` varchar(125) NOT NULL,
+  `admin_createdon` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `lms_admin`
+--
+
+INSERT INTO `lms_admin` (`admin_id`, `admin_email`, `admin_password`, `admin_username`, `admin_contact_no`, `admin_profile`, `admin_createdon`) VALUES
+(3, 'admin@gmail.com', '$2y$10$Ws1d5kbul3xgvby6gGoyUueoYet4jqhQkAsLa/OKynYNsLt8RTwba', 'Admin', '0250784957', '66a2639ee8e218.73594825.jpg', '2024-07-25 16:39:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lms_author`
+--
+
+CREATE TABLE `lms_author` (
+  `author_id` int(11) NOT NULL,
+  `author_name` varchar(200) NOT NULL,
+  `author_status` enum('Enable','Disable') NOT NULL,
+  `author_created_on` varchar(30) NOT NULL,
+  `author_updated_on` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lms_book`
+--
+
+CREATE TABLE `lms_book` (
+  `book_id` int(11) NOT NULL,
+  `book_code` varchar(150) NOT NULL,
+  `book_category` varchar(200) NOT NULL,
+  `book_author` varchar(200) NOT NULL,
+  `book_location_rack` varchar(100) NOT NULL,
+  `book_name` text NOT NULL,
+  `book_no_of_copy` int(5) NOT NULL,
+  `book_status` enum('Enable','Disable') NOT NULL,
+  `book_added_on` varchar(30) NOT NULL,
+  `book_updated_on` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lms_category`
+--
+
+CREATE TABLE `lms_category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(200) NOT NULL,
+  `category_status` enum('Enable','Disable') NOT NULL,
+  `category_created_on` varchar(30) NOT NULL,
+  `category_updated_on` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lms_issue_book`
+--
+
+CREATE TABLE `lms_issue_book` (
+  `issue_book_id` int(11) NOT NULL,
+  `book_id` varchar(30) NOT NULL,
+  `student_id` varchar(30) NOT NULL,
+  `issue_date_time` varchar(30) NOT NULL,
+  `expected_return_date` varchar(30) NOT NULL,
+  `return_date_time` varchar(30) NOT NULL,
+  `book_fines` varchar(30) NOT NULL,
+  `book_issue_status` enum('Issue','Return','Not Return') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lms_location_rack`
+--
+
+CREATE TABLE `lms_location_rack` (
+  `location_rack_id` int(11) NOT NULL,
+  `location_rack_name` varchar(200) NOT NULL,
+  `location_rack_status` enum('Enable','Disable') NOT NULL,
+  `location_rack_created_on` varchar(30) NOT NULL,
+  `location_rack_updated_on` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `lms_location_rack`
+--
+
+INSERT INTO `lms_location_rack` (`location_rack_id`, `location_rack_name`, `location_rack_status`, `location_rack_created_on`, `location_rack_updated_on`) VALUES
+(12, 'A1', 'Enable', '2024-07-25 15:52:37', '2024-07-25 22:31:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lms_setting`
+--
+
+CREATE TABLE `lms_setting` (
+  `setting_id` int(11) NOT NULL,
+  `library_name` varchar(200) NOT NULL,
+  `library_address` text NOT NULL,
+  `library_contact_number` varchar(30) NOT NULL,
+  `library_email_address` varchar(100) NOT NULL,
+  `library_total_book_issue_day` int(5) NOT NULL,
+  `library_one_day_fine` decimal(4,2) NOT NULL,
+  `library_issue_total_book_per_user` int(3) NOT NULL,
+  `library_currency` varchar(30) NOT NULL,
+  `library_timezone` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `lms_setting`
+--
+
+INSERT INTO `lms_setting` (`setting_id`, `library_name`, `library_address`, `library_contact_number`, `library_email_address`, `library_total_book_issue_day`, `library_one_day_fine`, `library_issue_total_book_per_user`, `library_currency`, `library_timezone`) VALUES
+(1, 'Btech Library', 'Accra Technical University', '0547894561', 'atulibrary@gmail.com', 10, 1.00, 3, 'GHC', 'Africa/Accra');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lms_user`
+--
+
+CREATE TABLE `lms_user` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(200) NOT NULL,
+  `student_id` varchar(125) NOT NULL,
+  `user_address` text NOT NULL,
+  `user_contact_no` varchar(30) NOT NULL,
+  `user_profile` varchar(100) NOT NULL,
+  `user_email_address` varchar(200) NOT NULL,
+  `user_password` varchar(225) NOT NULL,
+  `user_status` varchar(225) NOT NULL DEFAULT 'Enable',
+  `user_created_on` varchar(30) NOT NULL,
+  `user_updated_on` varchar(30) NOT NULL,
+  `student_faculty` varchar(225) NOT NULL,
+  `student_department` varchar(225) NOT NULL,
+  `student_program` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `lms_admin`
+--
+ALTER TABLE `lms_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `lms_author`
+--
+ALTER TABLE `lms_author`
+  ADD PRIMARY KEY (`author_id`);
+
+--
+-- Indexes for table `lms_book`
+--
+ALTER TABLE `lms_book`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `lms_category`
+--
+ALTER TABLE `lms_category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `lms_issue_book`
+--
+ALTER TABLE `lms_issue_book`
+  ADD PRIMARY KEY (`issue_book_id`);
+
+--
+-- Indexes for table `lms_location_rack`
+--
+ALTER TABLE `lms_location_rack`
+  ADD PRIMARY KEY (`location_rack_id`);
+
+--
+-- Indexes for table `lms_setting`
+--
+ALTER TABLE `lms_setting`
+  ADD PRIMARY KEY (`setting_id`);
+
+--
+-- Indexes for table `lms_user`
+--
+ALTER TABLE `lms_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `lms_admin`
+--
+ALTER TABLE `lms_admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `lms_author`
+--
+ALTER TABLE `lms_author`
+  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `lms_book`
+--
+ALTER TABLE `lms_book`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `lms_category`
+--
+ALTER TABLE `lms_category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `lms_issue_book`
+--
+ALTER TABLE `lms_issue_book`
+  MODIFY `issue_book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `lms_location_rack`
+--
+ALTER TABLE `lms_location_rack`
+  MODIFY `location_rack_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `lms_setting`
+--
+ALTER TABLE `lms_setting`
+  MODIFY `setting_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `lms_user`
+--
+ALTER TABLE `lms_user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
